88f00aa76281e3456177f9dd5d025fb5479e8145
DemonHunter.x64 <mv261905345@gmail.com>
2025-09-02 00:43:25 -0300

    Update servidor.py

--- meu servidor/servidor.py
+++ meu servidor/servidor.py
@@ -1,13 +1,1 @@
-import socket
 
-host = "177.52.51.71"  
-port = 5000
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
-
-while True:
-    msg = input("Digite algo: ")
-    client.send(msg.encode())
-    data = client.recv(1024).decode()
-    print("Resposta do servidor:", data)
